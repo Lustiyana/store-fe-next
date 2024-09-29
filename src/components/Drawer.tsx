4fdef2842e6e99b890608033b7e900dcc0cbf029
Lustiyana <lustiyana1801@gmail.com>
2024-09-30 00:32:32 +0700

    integrate with api

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -2,11 +2,9 @@
 
 "use client";
 import { menu } from "@/data/menu";
-import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React from "react";
-import Logo from "../../public/images/logo-store.png";
 
 const Drawer = () => {
   const pathname = usePathname();
@@ -18,15 +16,7 @@ const Drawer = () => {
         className="drawer-overlay"
       ></label>
       <ul className="menu  text-base-content min-h-full w-80 p-4 border-r">
-        <li className="text-center">
-          <Image
-            src={Logo}
-            alt="logo-store.png"
-            width={96}
-            height={96}
-            className="m-auto mb-8"
-          />
-        </li>
+        <li className="text-center font-bold text-3xl">MOStore</li>
         {menu.map((item, index) => (
           <li
             key={index}
